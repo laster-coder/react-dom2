21f345a6247f58e623f76c407fb29934b2ea76d4
unknown <ggx9912@163.com>
2023-05-27 00:18:54 +0800

    这是可以使用的井字棋

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,48 @@
-import React from"react";
-import {ReactRoot, createRoot}from "react-dom/client"
+import React, { useState } from"react";
+import { createRoot}from "react-dom/client"
 import "./index.css"
 
-const Cell=function(){
-	return <div className="cell"></div>
+const Cell = function(props){
+	return (<div className="cell" onClick={props.onClick}>
+		{props.text}
+		</div>)
 }
-const Cells=[
-	[null,null,null],
-	[null,null,null],
-	[null,null,null],
-]
 
-const Chessboard=function(){
-return(
-	<div>
-		{Cells.map((items)=>{
-			return(<div className="row">
-				{items.map((item)=>{
-					return <div className="cell">
-						<Cell text={item}/>
+
+const Chessboard =function(){
+	const [Cells, steCells]=useState([
+		[null,null,null],
+		[null,null,null],
+		[null,null,null],
+	])
+	const [n,setN]=useState(0)
+
+	const onClickCell =(row,col)=>{
+		console.log('onclickCell');
+		console.log('行'+row);
+		console.log('列'+col);
+		setN(n + 1)
+		console.log(n);
+		const copy =JSON.parse(JSON.stringify(Cells))
+		copy[row][col]= n % 2 == 0 ? 'X' : 'O'
+		steCells(copy)
+	}
+	return(
+		<div>
+			{Cells.map((items,row)=>{
+				return(
+					<div className="row">
+						{items.map((item,col)=>{
+							return<div className="cell">
+								<Cell text={item} onClick={()=>onClickCell(row,col)} /></div>
+						})}
 					</div>
-				})}
-			</div>)
-		})}
-	</div>
-)
+				)
+			})}
+		</div>
+	)
 }
+
 
 const container =document.getElementById("root")
 const root=createRoot(container)
