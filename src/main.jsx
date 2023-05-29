81fb8b26a274f771a38e9f7d6d7192a4ff14f2d6
unknown <ggx9912@163.com>
2023-05-29 18:29:05 +0800

    这是一个更新后的井字棋

--- src/main.jsx
+++ src/main.jsx
@@ -15,8 +15,38 @@ const Chessboard =function(){
 		[null,null,null],
 		[null,null,null],
 	])
+	const [finished,setFinished]=useState(false)
 	const [n,setN]=useState(0)
-
+    const tell=(cells)=>{
+	for(let i=0;i<3;i++){
+		if(cells[i][0]===cells[i][1]&&cells[i][1]===cells[i][2]&&
+			cells[i][0]!==null)
+			{console.log(cells[i][0]+'赢了')
+			setFinished(true)
+			break;
+		}
+	}
+	for(let i=0;i<3;i++){
+		if(cells[0][i]===cells[1][i]&&cells[1][i]===cells[2][i]&&
+			cells[0][i]!==null)
+			{console.log(cells[0][i]+'赢了')
+			setFinished(true)
+			break;
+		}
+	}
+	if(cells[0][0]===cells[1][1]&&
+		cells[1][1]===cells[2][2]&&
+		cells[0][0]!==null){
+			{console.log(cells[0][0]+'赢了')
+			setFinished(true)}
+		}
+		if(cells[0][2]===cells[1][1]&&
+			cells[1][1]===cells[2][0]&&
+			cells[1][1]!==null){
+				{console.log(cells[1][1]+'赢了')
+				setFinished(true)}
+			}
+}
 	const onClickCell =(row,col)=>{
 		console.log('onclickCell');
 		console.log('行'+row);
@@ -26,6 +56,7 @@ const Chessboard =function(){
 		const copy =JSON.parse(JSON.stringify(Cells))
 		copy[row][col]= n % 2 == 0 ? 'X' : 'O'
 		steCells(copy)
+		tell(copy)
 	}
 	return(
 		<div>
@@ -39,6 +70,7 @@ const Chessboard =function(){
 					</div>
 				)
 			})}
+			{finished && <div className="gameOver">游戏结束</div>}
 		</div>
 	)
 }
